58decf66edba33f3971c6a2ba38b1b933dd90565
smithforge <45121120+smithforge@users.noreply.github.com>
2018-11-23 11:18:54 +0800

    Add files via upload

---
+++ practise.py
@@ -0,0 +1,17 @@
+name="天hdkfjkasdkjhkssa田"
+#print(name[1])
+#print(len(name))
+#print(name[len(name)-8])
+#print(name[2:5])
+#print(name*2)
+#print(name+"2")
+
+
+my_list={'tm',',天天','田田','gigabyte'}
+# print(my_list[0])
+# print(len(my_list))
+# my_list.append(2000)
+# my_list.remove(my_list[2])
+print(my_list)
+my_dictionary={'Iphone':1000,'sumsang s9':900}
+print(my_dictionary)
